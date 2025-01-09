cc8001ba0a8f295a8f98748d29db581c98d926c8
SyuxCodeZ <cuberdytm@gmail.com>
2025-01-09 17:50:48 +0600

    kfklhsgljh;aagkjhafgkjhfgkjhgfjhkgkjhgfkhfggkafakghakghjgkjhlhjkghjgf

---
+++ Activity3.py
@@ -0,0 +1,9 @@
+#the input and the variable
+
+num = int(input("Enter The Number You Want To Reverse: "))
+
+#the for loop
+
+for i in range(num, 0, -1):
+
+    print (f"the original number is {i}")
